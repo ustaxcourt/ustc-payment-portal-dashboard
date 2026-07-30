162826ea6fde652a7e72ca6866c47857c8622ad7
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2026-07-30 08:40:59 -0700

    PAY-330: Rename amplify

--- terraform/modules/amplify/outputs.tf
+++ terraform/modules/amplify/outputs.tf
@@ -1,9 +1,9 @@
 output "app_id" {
   description = "Amplify app ID"
-  value       = aws_amplify_app.this.id
+  value       = aws_amplify_app.payment_portal_dashboard.id
 }
 
 output "default_domain" {
   description = "Amplify-provided domain, usable before the custom domain verifies"
-  value       = aws_amplify_app.this.default_domain
+  value       = aws_amplify_app.payment_portal_dashboard.default_domain
 }
