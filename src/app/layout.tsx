6882a13327461e816389193bded81e1a9eb17749
Franz Noel Tanglao <Franz.Tanglao.ctr@ustaxcourt.gov>
2026-08-03 07:54:40 -0700

    PAY-331 fix: Add the correct Provider directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "./Providers";
+import Providers from "./providers/Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
