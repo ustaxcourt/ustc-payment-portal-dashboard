034d264e8cb673f009c623588923d45aabfc0825
Franz Noel Tanglao <Franz.Tanglao.ctr@ustaxcourt.gov>
2026-08-05 12:04:00 -0700

    feat: Changed how the route handler works

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,6 @@ import NextAuth from "next-auth";
 import { getAuthOptions } from "@/lib/auth";
 
 const handler = async (request: Request, context: unknown) =>
-	NextAuth(getAuthOptions())(request, context);
+  NextAuth(getAuthOptions())(request, context);
 
 export { handler as GET, handler as POST };
