08e430f1d1ed4587f818984303c3b1a99d78260f
Franz Noel Tanglao <Franz.Tanglao.ctr@ustaxcourt.gov>
2026-08-11 07:41:19 -0700

    PAY-386 feat: Do not select buttons if Custom Range is selected

--- src/features/transaction-log/TimeframeControls.tsx
+++ src/features/transaction-log/TimeframeControls.tsx
@@ -94,7 +94,7 @@ export default function TimeframeControls({
             const isCustom = preset === "custom";
             const isSelected = isCustom
               ? isCustomOpen
-              : appliedRange.preset === preset;
+              : !isCustomOpen && appliedRange.preset === preset;
 
             return (
               <Button
